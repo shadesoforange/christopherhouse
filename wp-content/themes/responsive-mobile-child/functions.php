de0d7905abfca56b8ca87823d15752d25b5ae371
Ian Monk <ian@creativedepartment.com>
2014-12-17 02:56:55 -0500

    adding everything

--- wp-content/themes/responsive-mobile-child/functions.php
+++ wp-content/themes/responsive-mobile-child/functions.php
@@ -6,12 +6,14 @@ function enqueue_parent_theme_style() {
 }
 
 if ( function_exists( 'add_image_size' ) ) {
-add_image_size( 'column-third', 285, 190, true ); //(cropped)
+add_image_size( 'column-third', 285, 190, true ); // cropped
+add_image_size( 'full-width', 600, 9999 ); // unlimited height
 }
 add_filter('image_size_names_choose', 'my_image_sizes');
 function my_image_sizes($sizes) {
 $addsizes = array(
-"column-third" => __( "1/3 Column")
+"column-third" => __( "1/3 Column"),
+"full-width" => __( "Full Width")
 );
 $newsizes = array_merge($sizes, $addsizes);
 return $newsizes;
